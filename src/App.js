32f5f9041d4d068abfc5795f200ed45f8db24d4d
saron4 <saronfantahun9@gmail.com>
2020-04-18 17:12:54 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React , {Component } from 'react';
+import './resources/styles.css';
+
+import Header from './Components/header_footer/Header';
+
+class App extends Component{
+  render() {
+    return(
+      <div className = "App">
+        <Header/>
+    </div>
+    );
+    
+  }
+}
+
+
+export default App;
---
+++ src/Components/header_footer/Header.js
@@ -0,0 +1,26 @@
+import React , {Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import MenuIcon from '@material-ui/core/MenuIcon';
+// import IconButton from '@material-ui/core/IconButton';
+
+class Header extends Component{
+  render() {
+    return(
+      <div>
+          
+          <AppBar
+            position = "fixed"
+          
+          >
+
+          </AppBar>
+    </div>
+    );
+    
+  }
+}
+
+
+export default Header;
