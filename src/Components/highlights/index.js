3f97f05ad332f46992ad13f1397417a81dc331f1
saron4 <saronfantahun9@gmail.com>
2020-04-20 19:39:02 -0500

    add a highlight component

---
+++ src/Components/highlights/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Discription from './Discription';
+import Discount from './Discount';
+
+const Highlights = () => {
+    return (
+        <div className="highlight_wrapper"> 
+            <Discription/>
+            <Discount/>
+        </div>
+
+        
+    );
+};
+
+export default Highlights;
